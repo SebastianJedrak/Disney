0d243ec10d8222687605b0c3a00e2bbafa54634f
SebastianJedrak <jedrak.sebastian@gmail.com>
2023-08-03 18:27:24 +0200

    prepared and displayed star svg

--- js/index.ts
+++ js/index.ts
@@ -32,6 +32,11 @@ type favoritesType = {
   favorite: boolean;
 }[];
 
+//ICONS
+const emptyStar = '<svg fill="yellow" xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 -960 960 960" width="48"><path d="m323-205 157-94 157 95-42-178 138-120-182-16-71-168-71 167-182 16 138 120-42 178ZM233-80l65-281L80-550l288-25 112-265 112 265 288 25-218 189 65 281-247-149L233-80Zm247-355Z"/></svg>';
+
+const fillStar = '<svg fill="yellow" xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 -960 960 960" width="48"><path d="m233-80 65-281L80-550l288-25 112-265 112 265 288 25-218 189 65 281-247-149L233-80Z"/></svg>'
+
 //FETCH FROM LOCAL STORAGE
 const favorites: favoritesType = JSON.parse(localStorage.getItem("favorites")!);
 // const test = [{id: 14, favorite: true}];
@@ -118,6 +123,7 @@ function renderList(data: fetchResultsType["data"], htmlElement: Element) {
     <li>
     <img src="${element.imageUrl}" alt="${element.name}">
     <span>${element.name}</span>
+    ${fillStar}
     </li>`
     )
     .join(" ");

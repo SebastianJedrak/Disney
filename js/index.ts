6dd652694a7aa8d3b4a1a3206b8e2f17354694c7
SebastianJedrak <jedrak.sebastian@gmail.com>
2023-08-03 13:01:07 +0200

    fetched data from a  disney api

--- js/index.ts
+++ js/index.ts
@@ -1,1 +1,22 @@
-console.log("test");
+//FETCH DATA FROM API
+
+async function getData() {
+  const url = `https://api.disneyapi.dev/character?pageSize=100`;
+  
+  try {
+    // Fetch
+    const fetchJson = await fetch(url);
+    if (!fetchJson.ok)
+      throw new Error(`${fetchJson.status}: Something goes wrong`);
+    const data = await fetchJson.json();
+    console.log(data);
+    return data;
+
+  } catch (err) {
+    // Error Handle
+    console.log(err.message);
+
+  }
+}
+
+getData()

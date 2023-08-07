8df50f7bd25eb667a8a3493d24a3f1e3b5b910e2
SebastianJedrak <jedrak.sebastian@gmail.com>
2023-08-07 11:15:39 +0200

    moved render view to separate module

--- js/index.ts
+++ js/index.ts
@@ -1,17 +1,12 @@
 import { fetchResultsType, favoritesType } from "./types";
-import { emptyStar, fillStar } from "./icons";
+import { renderHtml } from "./view";
 
 //FETCH FROM LOCAL STORAGE
 const favorites: favoritesType = JSON.parse(localStorage.getItem("favorites")!);
 // const test = [{id: 14, favorite: true}];
 // localStorage.setItem("favorites", JSON.stringify(test))
 
-// DOCUMENT SELECTORS
-const allCharactersList = document.querySelector(".characters-all")!;
-const favoritesCharactersList = document.querySelector(
-  ".characters-favorites"
-)!;
-const topCharactersList = document.querySelector(".characters-top")!;
+
 
 //FETCH DATA FROM API
 
@@ -64,62 +59,4 @@ function dataFavoritesConcat(data: fetchResultsType) {
   return resultsFavoriteConcat;
 }
 
-//RENDER HTML
 
-function renderHtml(data: fetchResultsType["data"]) {
-  // Render all characters list
-  renderList(data, allCharactersList, "list");
-
-  // Render only favorite characters list
-  const onlyFavoritesFilter = data.filter(
-    (element) => element.isFavorite === true
-  );
-  renderList(onlyFavoritesFilter, favoritesCharactersList, "list");
-
-  // Render top3 character cards
-  const onlyTopThreeFilter = data
-    .sort((a, b) => b.films.length - a.films.length)
-    .slice(0, 3);
-  renderList(onlyTopThreeFilter, topCharactersList, "card");
-}
-
-//RENDER LIST OF FETCHED ITEMS
-
-/**
- * Render array of data in UI
- * @data array od data
- * @htmlElement parent html element to inject html
- * @type type of rendered element
- */
-function renderList(
-  data: fetchResultsType["data"],
-  htmlElement: Element,
-  type: "list" | "card"
-) {
-  const htmlToInject = data
-    .map(
-      (element) => `
-    <li>
-    <img src="${element.imageUrl}" alt="${element.name}">
-
-    <span>${element.name}</span>
-
-    ${
-      type === "list"
-        ? `<span>${"TV"}</span>`
-        : `<span>TV Shows:</span><span>${element.tvShows.length}</span>`
-    }
-
-    ${
-      type === "list"
-        ? `<span>${element.films.length}</span>`
-        : `<span>Films:</span><span>${element.films.length}</span>`
-    }
-    
-    ${element.isFavorite ? fillStar : emptyStar}
-    </li>`
-    )
-    .join(" ");
-
-  htmlElement.insertAdjacentHTML("afterbegin", htmlToInject);
-}
---
+++ js/view.ts
@@ -0,0 +1,69 @@
+import { fetchResultsType, favoritesType } from "./types";
+import { emptyStar, fillStar } from "./icons";
+
+// DOCUMENT SELECTORS
+const allCharactersList = document.querySelector(".characters-all")!;
+const favoritesCharactersList = document.querySelector(
+  ".characters-favorites"
+)!;
+const topCharactersList = document.querySelector(".characters-top")!;
+
+//RENDER HTML
+
+export function renderHtml(data: fetchResultsType["data"]) {
+  // Render all characters list
+  renderList(data, allCharactersList, "list");
+
+  // Render only favorite characters list
+  const onlyFavoritesFilter = data.filter(
+    (element) => element.isFavorite === true
+  );
+  renderList(onlyFavoritesFilter, favoritesCharactersList, "list");
+
+  // Render top3 character cards
+  const onlyTopThreeFilter = data
+    .sort((a, b) => b.films.length - a.films.length)
+    .slice(0, 3);
+  renderList(onlyTopThreeFilter, topCharactersList, "card");
+}
+
+//RENDER LIST OF FETCHED ITEMS
+
+/**
+ * Render array of data in UI
+ * @data array od data
+ * @htmlElement parent html element to inject html
+ * @type type of rendered element
+ */
+function renderList(
+  data: fetchResultsType["data"],
+  htmlElement: Element,
+  type: "list" | "card"
+) {
+  const htmlToInject = data
+    .map(
+      (element) => `
+      <li>
+      <img src="${element.imageUrl}" alt="${element.name}">
+  
+      <span>${element.name}</span>
+  
+      ${
+        type === "list"
+          ? `<span>${"TV"}</span>`
+          : `<span>TV Shows:</span><span>${element.tvShows.length}</span>`
+      }
+  
+      ${
+        type === "list"
+          ? `<span>${element.films.length}</span>`
+          : `<span>Films:</span><span>${element.films.length}</span>`
+      }
+      
+      ${element.isFavorite ? fillStar : emptyStar}
+      </li>`
+    )
+    .join(" ");
+
+  htmlElement.insertAdjacentHTML("afterbegin", htmlToInject);
+}
